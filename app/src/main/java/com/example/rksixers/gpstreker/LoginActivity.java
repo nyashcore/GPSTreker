54a74a0f717220341d10df5f7b065882ed8b9ff2
Said Gadjiev <gsarma.1995@mail.ru>
2016-02-19 22:56:38 +0300

    Login and Register Activity

--- app/src/main/java/com/example/rksixers/gpstreker/LoginActivity.java
+++ app/src/main/java/com/example/rksixers/gpstreker/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.rksixers.gpstreker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void loginButtonClick(View view) {
+        startActivity(new Intent(LoginActivity.this, SignupActivity.class));
     }
 }
